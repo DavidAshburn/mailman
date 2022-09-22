078ffc3bd858b63a2041031a6a75d42d8ccb8d42
DavidAshburn <oneinchwebbing@gmail.com>
2022-09-21 21:49:19 -0700

    styling and responsive navbar with collapsible links

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get 'home/about'
+  get 'home/profile'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
